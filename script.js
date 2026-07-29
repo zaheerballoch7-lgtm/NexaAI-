74e037f95b9a62b7b5f78ee10229f3271af0891b
zaheerballoch7-lgtm <zaheerballoch7@gmail.com>
2026-07-29 21:48:01 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "YOUR_GEMINI_API_KEY";
+const API_KEY = AQ.Ab8RN6INwM_tq3BIuG8s8_lT5ECjebrUAqFOQ-bcU5kPSnTW-g
 
 async function sendMessage() {
   const input = document.getElementById("userInput");
